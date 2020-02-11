86c6832a4343f38b58ad122c4931112a825b7a69
yuandaw <54134506+yuandaw@users.noreply.github.com>
2020-02-11 14:21:10 -0500

    Update SVM_classifier.m

--- SVM_classifier.m
+++ SVM_classifier.m
@@ -1,14 +1,24 @@
-%load your data file
-features=data(:,1:100);%features
-classlabel=data(:,101);%labels
-n = randperm(size(features,1));%generating training&testing set randomly
+class = 7 %number of classes
+sample = 400 %sample number for each class
+proportion = 0.1 %proportion of training set
+features=mobile(:,1:100);%features
+classlabel=mobile(:,101);%labels
+m(1,1:400) = randperm(400);%generating training&testing set randomly
+for i = 1:class
+    n(1,sample*proportion*(i-1)+1:sample*proportion*i) = m(1,1:sample*proportion)+sample*(i-1);
+end
 
-%% training set--400 samples
-train_features=features(n(1:400),:);
-train_label=classlabel(n(1:400),:);
-%% testing set--(total- 400)samples
-test_features=features(n(401:end),:);
-test_label=classlabel(n(401:end),:);
+
+for i = 1:class
+    l(1,sample*(1-proportion)*(i-1)+1:sample*(1-proportion)*i) = m(1,sample*proportion+1:sample)+sample*(i-1);
+end
+
+%% training set
+train_features=features(n(1:end),:);
+train_label=classlabel(n(1:end),:);
+%% testing set
+test_features=features(l(1:end),:);
+test_label=classlabel(l(1:end),:);
 
 %% Data normalization
  [Train_features,PS] = mapminmax(train_features');
@@ -30,3 +40,6 @@ fprintf('training set accuracy：%f\n',accuracy_train)
 compare_test = (test_label == predict_test_label);
 accuracy_test = sum(compare_test)/size(test_label,1)*100;
 fprintf('testing set accuracy：%f\n',accuracy_test)
+
+%%you must clear workspace and reload the data file when you change the
+%%proportion of training set
